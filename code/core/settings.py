1a7526fd9f65d56d4b03e64be8d9b81f6705464f
Pablo Alves Neri <pabloneri66@gmail.com>
2025-12-26 20:36:07 -0300

    Feat: Pagination

--- code/core/settings.py
+++ code/core/settings.py
@@ -124,3 +124,11 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+
+# Pag10
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+# limit=100&offset=400
